df8f4d3f34169e8ec3a88f644d603ebf211a105e
like <likeaixi@gmail.com>
2018-04-15 14:36:40 +0800

    修改webpack配置

--- build/webpack.com.config.js
+++ build/webpack.com.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|jpg|gif)$/,
                 use: [
                     {
                         loader: 'file-loader',
